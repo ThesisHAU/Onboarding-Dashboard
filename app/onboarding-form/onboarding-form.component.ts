89b47ea12ffc58c2223adf6aae4dd32d5fa031b3
Louise <114331690+BlankuZero@users.noreply.github.com>
2025-01-18 15:53:44 +0800

    Add files via upload

---
+++ app/onboarding-form/onboarding-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding-form',
+  standalone: false,
+  
+  templateUrl: './onboarding-form.component.html',
+  styleUrl: './onboarding-form.component.css'
+})
+export class OnboardingFormComponent {
+
+}
